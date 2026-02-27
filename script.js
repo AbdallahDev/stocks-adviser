0ceb1761daab448711c6d1b3ee06b5e53a153dfe
AbdallahDev <abdallah.dev85@gmail.com>
2026-02-27 22:38:56 +0300

    stage

---
+++ script.js
@@ -0,0 +1,101 @@
+const mainEl = document.querySelector("main")
+
+function makeInputArea() {
+    let tickers = ""
+    const inputAreaEl = document.createElement('div')
+    inputAreaEl.id = 'inputArea'
+    inputAreaEl.innerHTML = `
+        <p id="details">Add up to 3 stock tickers below to
+            get a super accurate stock
+            predictins report 👇
+        </p>`
+
+    const tickerToolEl = document.createElement('div')
+    tickerToolEl.id = "tickerTool"
+
+    const tickerInputEl = document.createElement('input')
+    tickerInputEl.type = "text"
+    tickerInputEl.id = 'tickerInput'
+    tickerInputEl.placeholder = "AAPL"
+
+    const crossEl = document.createElement('div')
+    crossEl.id = 'cross'
+    crossEl.textContent = "+"
+    crossEl.addEventListener('click', () => {
+        const value = tickerInputEl.value.trim()
+        if (value) {
+            tickers += value + ", "
+            tickerTxtEl.textContent = tickers
+            tickerInputEl.value = ""
+            uploadBtnEl.disabled = false
+        }
+    })
+
+    const tickerTxtEl = document.createElement('p')
+    tickerTxtEl.id = 'tickerTxt'
+    tickerTxtEl.textContent = "Your tickers will appears here..."
+
+    const uploadBtnEl = document.createElement("button")
+    uploadBtnEl.id = "uploadBtn"
+    uploadBtnEl.textContent = "GENERATE REPORT"
+    if (tickers === "") uploadBtnEl.disabled = true
+    uploadBtnEl.addEventListener('click', () => {
+        showData(tickers)
+    })
+
+    tickerToolEl.append(tickerInputEl, crossEl)
+
+    const noteTxtEl = document.createElement("p")
+    noteTxtEl.id = 'noteTxt'
+    noteTxtEl.textContent = "Always correct 15% of the time!"
+
+    inputAreaEl.append(tickerToolEl, tickerTxtEl, uploadBtnEl, noteTxtEl)
+    mainEl.appendChild(inputAreaEl)
+}
+
+function getData(tickers) {
+    makeLoadingArea()
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(tickers)
+        }, 5000)
+    })
+}
+
+function makeLoadingArea() {
+    mainEl.innerHTML = `
+        <div id="loadingArea">
+            <div id="loadingIcon">
+                <img src="images/loader.gif" alt="loader gif">
+            </div>
+            <div id="loadingTxt">Creating report...</div>
+        </div>`
+}
+
+async function showData(tickers) {
+    try {
+        const result = await getData(tickers)
+        makeReportArea(result)
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+function makeReportArea(tickers) {
+    const reportAreaEl = document.createElement('div')
+    reportAreaEl.id = 'reportArea'
+
+    const titleEl = document.createElement('p')
+    titleEl.id = "title"
+    titleEl.textContent = "Your Report 💹"
+
+    const reportEl = document.createElement("p")
+    reportEl.id = "report"
+    reportEl.textContent = tickers
+
+    reportAreaEl.append(titleEl, reportEl)
+    mainEl.innerHTML = ""
+    mainEl.appendChild(reportAreaEl)
+}
+
+makeInputArea()
